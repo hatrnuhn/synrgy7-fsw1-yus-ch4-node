cbaf8b57332dbdd843e58f850d9b30fc2300216d
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-04-25 20:14:08 +0700

    import to main

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-// prisma
-// balok
-// kubus
+const {calcPrismVol, calcPrismSurfaceArea} = require('./prism.js');
+const {calcBlockVol, calcBlockSurfaceArea} = require('./block.js');
+const {calcCubeVol, calcCubeSurfaceArea} = require('./cube.js');
+
